1046bdf721a4f6443ab0c0f39f55c76aaafc08d9
Jordan Trask <jtrask@gmail.com>
2022-01-19 20:35:20 +0000

    Updated

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # wordpress-code-snippets
+A collection of WordPress PHP code snippets for use in WP Codebox because it's awesome :)
---
+++ woocommerce-order-search-in-admin-bar.php
@@ -0,0 +1,31 @@
+<?php 
+
+// Add shop order search in the admin bar
+add_action('wp_before_admin_bar_render', function() {
+  global $wp_admin_bar;
+
+  $search_query = '';
+  if (!empty($_GET['post_type']) && $_GET['post_type'] == 'shop_order' ) {
+
+    $search_query = !empty($_GET['s']) ? $_GET['s'] : '';
+
+    if($search_query) {
+        $order = get_post($search_query);
+
+        if($order) {
+            wp_redirect(get_edit_post_link($order->ID, ''));
+            exit;
+        }
+    }
+  }
+
+  $wp_admin_bar->add_menu(array(
+    'id' => 'admin_bar_shop_order_form',
+    'title' => '<form method="get" action="'.get_site_url().'/wp-admin/edit.php?post_type=shop_order">
+      <input name="s" type="text" placeholder="Order or Sub ID" value="' . $search_query . '" style="width:100px;height: 10px;padding-left: 5px;">
+      <button class="button button-primary" style="padding: 0px 10px 0px 10px!important; height: 5px!important;">Check</button>
+      <input name="post_type" value="shop_order" type="hidden">
+    </form>'
+  ));
+},100
+);
