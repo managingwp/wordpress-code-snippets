1c17d4280cdcfb6ee38cba188b7e4104e45ecf4d
Jordan <jtrask@gmail.com>
2022-01-20 20:56:04 -0500

    Update woocommerce-order-search-in-admin-bar.php

--- woocommerce-order-search-in-admin-bar.php
+++ woocommerce-order-search-in-admin-bar.php
@@ -1,8 +1,14 @@
 <?php 
 // Credit - https://stackoverflow.com/questions/48629362/wordpress-woocommerce-shop-order-search-in-admin-bar
 // Add shop order search in the admin bar
+// Fixes by Ovidiu Maghetiu from https://wpcodebox.com/
 
 add_action('wp_before_admin_bar_render', function() {
+
+  if(!current_user_can('administrator')) {
+    return;
+  }
+  
   global $wp_admin_bar;
 
   $search_query = '';
@@ -11,7 +17,7 @@ add_action('wp_before_admin_bar_render', function() {
     $search_query = !empty($_GET['s']) ? $_GET['s'] : '';
 
     if($search_query) {
-        $order = get_post($search_query);
+        $order = get_post(intval($search_query));
 
         if($order) {
             wp_redirect(get_edit_post_link($order->ID, ''));
@@ -23,7 +29,7 @@ add_action('wp_before_admin_bar_render', function() {
   $wp_admin_bar->add_menu(array(
     'id' => 'admin_bar_shop_order_form',
     'title' => '<form method="get" action="'.get_site_url().'/wp-admin/edit.php?post_type=shop_order">
-      <input name="s" type="text" placeholder="Order or Sub ID" value="' . $search_query . '" style="width:100px;height: 10px;padding-left: 5px;">
+      <input name="s" type="text" placeholder="Order or Sub ID" value="' . esc_attr($search_query) . '" style="width:100px;height: 10px;padding-left: 5px;">
       <button class="button button-primary" style="padding: 0px 10px 0px 10px!important; height: 5px!important;">Check</button>
       <input name="post_type" value="shop_order" type="hidden">
     </form>'
