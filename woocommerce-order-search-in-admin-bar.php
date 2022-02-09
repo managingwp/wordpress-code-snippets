c7489af45a1dde3cd2c6e089f7c289e839e5d6d2
Jordan <jtrask@gmail.com>
2022-02-09 14:32:51 -0500

    Update woocommerce-order-search-in-admin-bar.php

--- woocommerce-order-search-in-admin-bar.php
+++ woocommerce-order-search-in-admin-bar.php
@@ -2,37 +2,39 @@
 // Credit - https://stackoverflow.com/questions/48629362/wordpress-woocommerce-shop-order-search-in-admin-bar
 // Add shop order search in the admin bar
 // Fixes by Ovidiu Maghetiu from https://wpcodebox.com/
+// Copied from https://gist.github.com/ovidiumght/9a47917169b003dba50114c6b6fd30ed?fbclid=IwAR0iw_fbJAvBeKSxPKlAMUnm8M1N7cZzOTMSKj0LCElnnyFFk5-XVfzIscs
 
 add_action('wp_before_admin_bar_render', function() {
+    // Get current screen
+    $screen = get_current_screen();
 
-  if(!current_user_can('administrator')) {
-    return;
-  }
-  
-  global $wp_admin_bar;
-
-  $search_query = '';
-  if (!empty($_GET['post_type']) && $_GET['post_type'] == 'shop_order' ) {
-
-    $search_query = !empty($_GET['s']) ? $_GET['s'] : '';
-
-    if($search_query) {
-        $order = get_post(intval($search_query));
-
-        if($order) {
-            wp_redirect(get_edit_post_link($order->ID, ''));
-            exit;
-        }
+    // Check if current user is an administrator
+    if( !current_user_can('administrator') ) {
+        return;
     }
-  }
+    
 
-  $wp_admin_bar->add_menu(array(
+    
+    global $wp_admin_bar;
+
+    $search_query = '';
+    $search_query = !empty($_POST['order_search']) ? $_POST['order_search'] : '';
+    $search_query = intval($search_query);
+    
+        if($search_query) {
+            wp_redirect('edit.php?post_type=shop_order&s=' . $search_query);
+        }
+
+
+
+    $wp_admin_bar->add_menu(array(
     'id' => 'admin_bar_shop_order_form',
-    'title' => '<form method="get" action="'.get_site_url().'/wp-admin/edit.php?post_type=shop_order">
-      <input name="s" type="text" placeholder="Order or Sub ID" value="' . esc_attr($search_query) . '" style="width:100px;height: 10px;padding-left: 5px;">
-      <button class="button button-primary" style="padding: 0px 10px 0px 10px!important; height: 5px!important;">Check</button>
-      <input name="post_type" value="shop_order" type="hidden">
+    'title' => '<form method="post" action="">
+    <input name="order_search" type="text" placeholder="Order or Sub ID" style="width:100px;height: 10px;padding-left: 5px;">
+    <button class="button button-primary" style="padding: 0px 10px 0px 10px!important; height: 5px!important;">Check</button>
+    <input name="post_type" value="shop_order" type="hidden">
     </form>'
-  ));
+    ));
+    
 },100
 );
