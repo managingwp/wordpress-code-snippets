5f2e48c7bfcbe7f03eea752be0874be211c4d562
Jordan Trask <jtrask@gmail.com>
2024-06-25 19:56:21 -0400

    Created woocommerce-wc_product_loop_transient-expiration-to-1-day.php Moved all WooCommerce snippets to woocommerce folder

---
+++ woocommerce/woocommerce-wc_product_loop_transient-expiration-to-1-day.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Credit: https://github.com/mainwp/Code-Snippets-Functions/blob/master/Execute%20a%20function%20on%20a%20child%20site/WooCommerce/change-wc_product_loop_transient-expiration-to-1-day.txt
+*/
+
+add_action( 'setted_transient', 'mmx_wc_product_loop_transient', 50, 3 );
+function mmx_wc_product_loop_transient( $transient, $value, $expiration ){
+	$pos = strpos( $transient, 'wc_product_loop_' );
+	if ( $pos !== false && $expiration == 2592000 ) {
+		set_transient( $transient, $value, DAY_IN_SECONDS );
+	}
+}
